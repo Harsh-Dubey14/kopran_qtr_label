2ecdba618f768ad8506a6200bbbf0f84fdd72277
Harsh-Dubey14 <harsh.dubey@expoundtechnivo.com>
2026-02-05 15:57:19 +0530

    Update baseURL and credentials for QUALITY and PRODUCTION environments

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -12,19 +12,19 @@ switch (process.env.DEPLOYMENT_ENV) {
   case "QUALITY":
     auth_username = "ENGINEERING_PROD";
     auth_password = "7U6frD$)+B>mc)D{>>pgK2-5)yMENT+xBWe3Lp~K";
-    baseURL = "https://my430774-api.s4hana.cloud.sap";
+    baseURL = "https://my431480-api.s4hana.cloud.sap";
     break;
 
   case "PRODUCTION":
   default:
-    auth_username = "ENGINEERING_PROD";
-    auth_password = "g-eKKCVqBTUBXKXFNvAqTqMXocHtqHoXl2VylrqW";
-    baseURL = "https://my430774-api.s4hana.cloud.sap";
+    auth_username = "BAS_USER";
+    auth_password = `6a(8S~df6JZ=hYDB)KgtZ(ShWc4-7+j<QCp2W8(}`;
+    baseURL = "https://my433016-api.s4hana.cloud.sap";
     break;
 }
 
 const auth = Buffer.from(`${auth_username}:${auth_password}`).toString(
-  "base64"
+  "base64",
 );
 
 module.exports = {
